98b1477685d9b4ca26ec00c1b41d62694d323996
Samu <127152668+WiniciusNeves@users.noreply.github.com>
2023-09-20 15:19:37 -0300

    nome2.0.js

---
+++ nome2.0.js
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body{
+            font: normal 20pt arial ;
+            color: gold;
+        }
+        
+
+    </style>
+    
+</head>
+<body>
+    <script>
+        var nome = prompt("qual e sou nome")
+        document.write(`Seu nome tem <strong>${nome} </strong>e seu nome tem lentras <strong>${nome.length}</strong></br> `)
+        document.write(`Seu nome em maiusculas é ${nome.toUpperCase()}</br>`)
+        document.write(`Seu nome em minucula é ${nome.toLowerCase()}</br>`)
+
+
+    </script>
+</body>
+</html>
