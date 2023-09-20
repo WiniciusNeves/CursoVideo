38f51782c58b01e2af3702a5b0fb0e9e013e0fb1
Samu <127152668+WiniciusNeves@users.noreply.github.com>
2023-09-20 15:18:39 -0300

    nome.js

---
+++ nome.js
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meu primeiro programa em JS</title>
+    <style>
+        body{
+            background-color: hwb(180 30% 17%);
+            color: white; /*isso aqui e um comentario em css*/
+            font:  normal 15pt Arial;
+        }
+        h1{
+            color: hsla(240, 68%, 48%, 0.534);
+        }
+
+    </style>
+</head>
+<body>
+    <script>
+       var nome = prompt("qual e sou nome jovem jedai") 
+       alert("É um grande prazer em te conhecer," + nome )
+    </script>
+</body>
+</html>
